9e29eb0ea58c5e96682cd05f5d8a1db004402c49
Pavlo Nahurnyi <polusnp@gmail.com>
2023-12-27 12:59:40 +0200

    about page in progress

--- src/components/StartInfo.jsx
+++ src/components/StartInfo.jsx
@@ -2,7 +2,7 @@ import { FaRotateRight, FaRotateLeft } from "react-icons/fa6"
 
 function StartInfo() {
   return (
-    <div className="absolute top-72 left-0 right-0 z-10 flex items-center justify-center opacity-90 ">
+    <div className="absolute top-72 left-0 right-0 z-10 flex items-center justify-center">
       <FaRotateLeft className="text-blue-800 animate-pulse" />
       <h1 className="sm:text-xl sm:leading-snug text-center px-6 text-blue-800 animate-pulse">
         DRAG TO EXPLORE
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,39 @@
-import React from "react"
+import { skills } from "../constants"
 
 function About() {
-  return <div>About</div>
+  return (
+    <section className="max-container-about font-poppins">
+      <h1 className="head-text ">
+        Hello, I'm{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow-sm">
+          Pavlo Nahurnyi
+        </span>
+      </h1>
+      <div>
+        <p className="mt-5 flex flex-col gap-3 text-blue-100">
+          React Developer from Ukraine with an industrial engineering background
+          in the automotive industry.
+        </p>
+      </div>
+
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text">My skills</h3>
+        <div className="mt-16 flex flex-wrap gap-12">
+          {skills.map((skill) => (
+            <div className="block-container w-16 h-16">
+              <div className="btn-front rounded-xl flex justify-center items-center">
+                <img
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default About
