7927583d9cfb03773d5954d0ad273cb0cadcbe85
Pavlo Nahurnyi <polusnp@gmail.com>
2024-01-04 23:50:27 +0200

    home page complete

--- src/components/StartInfo.jsx
+++ src/components/StartInfo.jsx
@@ -2,12 +2,12 @@ import { FaRotateRight, FaRotateLeft } from "react-icons/fa6"
 
 function StartInfo() {
   return (
-    <div className="absolute top-[325px] left-0 right-0 z-10 flex items-center justify-center">
-      <FaRotateLeft className="text-slate-200 animate-pulse" />
-      <h1 className="sm:text-2xl text-lg sm:leading-snug text-center px-6 text-slate-200 animate-pulse">
+    <div className="absolute top-[325px] left-0 right-0 z-10 flex items-center justify-center animate-pulse">
+      <FaRotateLeft className="text-slate-200 sm:text-xl text-base" />
+      <h1 className="sm:text-2xl text-lg sm:leading-snug text-center px-4 text-slate-200">
         DRAG TO EXPLORE
       </h1>
-      <FaRotateRight className="text-slate-200 animate-pulse" />
+      <FaRotateRight className="text-slate-200 sm:text-xl text-base" />
     </div>
   )
 }
